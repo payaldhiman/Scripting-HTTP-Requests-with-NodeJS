e667c9b6d51b66d584a28f618d4c06a80fa5a6ca
Payal Dhiman <payaldhiman@outlook.com>
2017-12-04 21:45:18 +0000

    Step 4

---
+++ step4.js
@@ -0,0 +1,36 @@
+function getHTML (options, callback) {
+  var https = require('https');
+
+
+
+  var arr = " " ;
+
+  https.get(options, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      arr += data + '/';
+      callback(data);
+
+    });
+
+    response.on('end', function() {
+
+      console.log('Response stream complete.');
+    });
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+
+getHTML(requestOptions, printHTML);
